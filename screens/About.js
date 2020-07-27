b5ffcd2a8873803a185a4228cd60e9ad02543c4a
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-07-27 14:37:12 -0400

    Added if statment for data to show on initial load

--- screens/About.js
+++ screens/About.js
@@ -3,15 +3,17 @@ import { View, Text, StyleSheet, Button } from "react-native";
 // import {globalStyles} from '../styles/global'
 
 export default function About({ navigation, route }) {
-  const { item } = route.params;
+  //   const { item } = route.params;
   console.log(route, "route in about");
 
   return (
     <View>
       <Text>About Screen</Text>
       <Button title="Home page" onPress={() => navigation.push("Home")} />
-      {/* <Text>{route.params.item.title}</Text> */}
-      {route.params ? <Text>{item.title}</Text> : null}
+      {/* <Text> {route.params.item.title}</Text> */}
+      {route.params ? <Text> {route.params.item.title}</Text> : null}
+
+      {/* {route.params ? <Text>{item.title}</Text> : null} */}
     </View>
   );
 }
