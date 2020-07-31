4ff4904a73caee24f5e1a111390b47015d9a4070
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-07-31 09:32:49 -0400

    Added all details data to render in about screen

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function ReviewDetails({ navigation, route }) {
+
+  if (!route.params) <Text>...loading</Text>;
   return (
     <View>
       <Text>Review Details Screen</Text>
       <Button title="Home page" onPress={() => navigation.push("Home Page")} />
-      {route.params ? <Text> {route.params.item.title}</Text> : null}
-
-     
+      {/* {route.params ? <Text> {route.params.item.title}</Text>      
+ : null} */}
+ <View >
+      <Text> {route.params.item.title}</Text>
+      <Text> {route.params.item.body}</Text>
+      <Text> Rating:{route.params.item.rating}</Text>
+      </View>
+      {/* <View style={container}>
+    <Text style={text}>React Native</Text>
+  </View> */}
     </View>
   );
 }
+
+const styles=StyleSheet.create({
+container:{
+    padding:24,
+}
+});
